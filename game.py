e6a37ece588b95d47161c0ad791d74dbc76a8045
Adam Ousherovitch <adamoosya@berkeley.edu>
2023-09-04 19:49:37 -0700

    updated player model and added colors

--- game.py
+++ game.py
@@ -1,24 +1,42 @@
 import numpy as np
 import networkx as nx
 import matplotlib.pyplot as plt
+import random
+
 
 class Game:
     def __init__(self, graph_type='random'):
         self.graph = Graph()
-        self.min_player = ...
-        self.max_player = ...
-        self.curr_vertex = ... # random vertex
-        
+        # TODO: actually well define min and max
+        self.min_player = DefaultPlayer(True)
+        self.max_player = DefaultPlayer(False)
+        self.curr_vertex = random.choice(
+            list(self.graph.graph.nodes))  # random vertex
+        self.graph.set_node_color(self.curr_vertex, 'red')
+
     def display(self):
         self.graph.display()
 
+    def turn(self):
+        advantage = random.randint(0, 1)
+        self.graph.set_node_color(self.curr_vertex, 'skyblue')
+        if advantage == 1:
+            self.curr_vertex = self.max_player.strategy(
+                self.graph, self.curr_vertex)
+
+        else:
+            self.curr_vertex = self.min_player.strategy(
+                self.graph, self.curr_vertex)
+        self.graph.set_node_color(self.curr_vertex, 'red')
+
 
 class Graph:
-    #TODO: add attributes to each vertex --> representing value of boundary vertices
-    def __init__(self, n = 10, p = 0.1):
+    # TODO: add attributes to each vertex --> representing value of boundary vertices
+    def __init__(self, n=10, p=0.1):
+        self.node_colors = {}
         # N: Number of nodes in the graph
         # P: Desired probability of an edge between any two nodes for connectivity
-        
+
         # Create an initially empty graph
         self.graph = nx.Graph()
         # Add nodes
@@ -35,16 +53,30 @@ class Graph:
 
     def display(self):
         # Draw the graph using NetworkX and Matplotlib
-        nx.draw(self.graph, with_labels=True, node_size=500, node_color='skyblue', font_size=10, font_color='black')
+        colors = [self.node_colors[node]
+                  if node in self.node_colors else 'skyblue' for node in self.graph.nodes]
+
+        nx.draw(self.graph, with_labels=True, node_size=500,
+                node_color=colors, font_size=10, font_color='black')
         plt.title("Connected Random Graph")
         plt.show()
+
+    def set_node_color(self, node, color):
+        self.node_colors[node] = color
+
 
 class DefaultPlayer():
     def __init__(self, min_or_max):
         self.objective = min_or_max
 
     def strategy(self, graph, curr_vertex) -> int:
-        pass
+        # default strat is just choose a random neighbor
+        neighbors = list(graph.neighbors(curr_vertex))
+        if neighbors:
+            return random.choice(neighbors)
+        else:
+            return None
+
 
 # main
 for _ in range(10):
