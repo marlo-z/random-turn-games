3224c18b5eeff796eb402906e6deec9135a19404
Neo Lee <neo.lky852@gmail.com>
2023-11-08 20:40:40 -0800

    an easier way to draw the path

--- functions.py
+++ functions.py
@@ -1,28 +1,24 @@
 import numpy as np
 
-def get_bounds(map: np.ndarray[tuple[int, int], float]) \
-    -> list[tuple[int, int]]:
+def get_bounds(map: np.ndarray[tuple[int, int]]) -> list[tuple[int, int]]:
     bounds = []
     for i in range(map.shape[0]):
         for j in range(map.shape[1]):
-            if map[i, j] != np.inf:
+            if map[i, j] != np.inf and map[i, j] != -np.inf:
                 bounds.append((i, j))
     
     return bounds
             
 
-def uniform_random(map: np.ndarray[tuple[int, int], float],
-                   boundary_coords: list[tuple[int, int]]) \
-                    -> np.ndarray[tuple[int, int], float]:
-    max_val = np.max([map[i, j] for i, j in boundary_coords])
-    min_val = np.min([map[i, j] for i, j in boundary_coords])
+def uniform_random(map: np.ndarray[tuple[int, int], float]) -> np.ndarray:
+    max_val = np.ma.masked_invalid(map).max()
+    min_val = np.ma.masked_invalid(map).min()
     map_copy = map.copy()
     for i in range(map.shape[0]):
         for j in range(map.shape[1]):
-            if map[i, j] == np.inf or (i, j) in boundary_coords:
-                continue
-            map_copy[i, j] = min_val + max(
-                (max_val - min_val) * np.random.random(),
-                1e-4)
+            if map[i, j] == -np.inf:
+                map_copy[i, j] = min_val + max(
+                    (max_val - min_val) * np.random.random(),
+                    1e-4)
     
     return map_copy
