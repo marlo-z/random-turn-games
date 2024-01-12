1936b436dbee98b16430738bc13dbc1b31059277
Adam Ousherovitch <adamoosya@berkeley.edu>
2024-01-11 17:14:57 -0800

    rounding and parallelism

--- mina_margin_map/functions.py
+++ mina_margin_map/functions.py
@@ -17,35 +17,49 @@ def round_down(number, decimals=10):
     multiplier = 10 ** decimals
     return math.floor(number * multiplier) / multiplier
 
+
 ''' Basis functions '''
+
+
 def w(x):
     return (8 * x + 1) ** (1/2)
+
 
 def upw(x):
     return round_up(w(x))
 
+
 def downw(x):
     return round_down(w(x))
+
 
 def s(x):
     return ((w(x) - 1) ** 2) / (4 * (w(x) + 7))
 
+
 def ups(x):
-    return ((upw(x) - 1) ** 2) / (4 * (downw(x) + 7))
+    return round_up(((upw(x) - 1) ** 2) / (4 * (downw(x) + 7)))
+
 
 def downs(x):
-    return ((downw(x) - 1) ** 2) / (4 * (upw(x) + 7))
+    return round_down(((downw(x) - 1) ** 2) / (4 * (upw(x) + 7)))
+
 
 def sm(x):
     return 1 / s(1 / x)
 
+
 def upsm(x):
-    return 1 / downs(1 / x)
+    return round_up(1 / downs(1 / x))
+
 
 def downsm(x):
-    return 1 / ups(1 / x)
+    return round_down(1 / ups(1 / x))
+
 
 '''Indexed basis functions'''
+
+
 def si(x, i=0):
     if i == 0:
         return x
@@ -55,6 +69,7 @@ def si(x, i=0):
     else:
         inner = si(x, i + 1)
         return sm(inner)
+
 
 def upsi(x, i=0):
     if i == 0:
@@ -66,6 +81,7 @@ def upsi(x, i=0):
         inner = upsi(x, i + 1)
         return upsm(inner)
 
+
 def downsi(x, i=0):
     if i == 0:
         return x
@@ -76,32 +92,37 @@ def downsi(x, i=0):
         inner = downsi(x, i + 1)
         return downsm(inner)
 
+
 def ci(x, i=0):
     inner = si(x, i)
     return ((w(inner) + 3) ** 2) / 16
 
+
 def upci(x, i=0):
     inner = upsi(x, i)
-    return ((upw(inner) + 3) ** 2) / 16
+    return round_up(((upw(inner) + 3) ** 2) / 16)
+
 
 def downci(x, i=0):
     inner = downsi(x, i)
-    return ((downw(inner) + 3) ** 2) / 16
+    return round_down(((downw(inner) + 3) ** 2) / 16)
 
 
 def di(x, i=0):
     inner = si(x, i)
     return ((w(inner) + 3) ** 2) / (8 * (w(inner) + 1))
 
+
 def updi(x, i=0):
     innerup = upsi(x, i)
     innerdown = downsi(x, i)
-    return ((upw(innerup) + 3) ** 2) / (8 * (downw(innerdown) + 1))
+    return round_up(((upw(innerup) + 3) ** 2) / (8 * (downw(innerdown) + 1)))
+
 
 def downdi(x, i=0):
     innerup = upsi(x, i)
     innerdown = downsi(x, i)
-    return ((downw(innerdown) + 3) ** 2) / (8 * (upw(innerup) + 1))
+    return round_down(((downw(innerdown) + 3) ** 2) / (8 * (upw(innerup) + 1)))
 
 
 '''Memoized indexed basis functions'''
@@ -155,13 +176,15 @@ def Pi(x, i=0):
     else:
         return Pi(x, i - 1) + math.prod([ci(x, j) - 1 for j in range(0, i)])
 
+
 def upPi(x, i=0):
     if i < 0:
         raise ValueError("i must be greater than or equal to 0")
     elif i == 0:
         return 1
     else:
-        return upPi(x, i - 1) + math.prod([upci(x, j) - 1 for j in range(0, i)])
+        return upPi(x, i - 1) + round_up(math.prod([upci(x, j) - 1 for j in range(0, i)]))
+
 
 def downPi(x, i=0):
     if i < 0:
@@ -169,7 +192,7 @@ def downPi(x, i=0):
     elif i == 0:
         return 1
     else:
-        return downPi(x, i - 1) + math.prod([downci(x, j) - 1 for j in range(0, i)])
+        return downPi(x, i - 1) + round_down(math.prod([downci(x, j) - 1 for j in range(0, i)]))
 
 
 def Si(x, i=0):
@@ -180,13 +203,15 @@ def Si(x, i=0):
     else:
         return Si(x, i - 1) + math.prod([di(x, j) - 1 for j in range(0, i)])
 
+
 def upSi(x, i=0):
     if i < 0:
         raise ValueError("i must be greater than or equal to 0")
     elif i == 0:
         return 1
     else:
-        return upSi(x, i - 1) + math.prod([updi(x, j) - 1 for j in range(0, i)])
+        return upSi(x, i - 1) + round_up(math.prod([updi(x, j) - 1 for j in range(0, i)]))
+
 
 def downSi(x, i=0):
     if i < 0:
@@ -194,7 +219,7 @@ def downSi(x, i=0):
     elif i == 0:
         return 1
     else:
-        return downSi(x, i - 1) + math.prod([downdi(x, j) - 1 for j in range(0, i)])
+        return downSi(x, i - 1) + round_down(math.prod([downdi(x, j) - 1 for j in range(0, i)]))
 
 
 def Qi(x, i=0):
@@ -204,14 +229,16 @@ def Qi(x, i=0):
         return 0
     else:
         return Qi(x, i - 1) + math.prod([1 / (ci(x, -j) - 1) for j in range(1, i)])
-    
+
+
 def upQi(x, i=0):
     if i < 0:
         raise ValueError("i must be greater than or equal to 0")
     elif i == 0 or i == 1:
         return 0
     else:
-        return upQi(x, i - 1) + math.prod([1 / (downci(x, -j) - 1) for j in range(1, i)])
+        return upQi(x, i - 1) + round_up(math.prod([1 / (downci(x, -j) - 1) for j in range(1, i)]))
+
 
 def downQi(x, i=0):
     if i < 0:
@@ -219,7 +246,7 @@ def downQi(x, i=0):
     elif i == 0 or i == 1:
         return 0
     else:
-        return downQi(x, i - 1) + math.prod([1 / (upci(x, -j) - 1) for j in range(1, i)])
+        return downQi(x, i - 1) + round_down(math.prod([1 / (upci(x, -j) - 1) for j in range(1, i)]))
 
 
 def Ti(x, i=0):
@@ -230,13 +257,15 @@ def Ti(x, i=0):
     else:
         return Ti(x, i - 1) + math.prod([1 / (di(x, -j) - 1) for j in range(1, i)])
 
+
 def upTi(x, i=0):
     if i < 0:
         raise ValueError("i must be greater than or equal to 0")
     elif i == 0 or i == 1:
         return 0
     else:
-        return upTi(x, i - 1) + math.prod([1 / (downdi(x, -j) - 1) for j in range(1, i)])
+        return upTi(x, i - 1) + round_up(math.prod([1 / (downdi(x, -j) - 1) for j in range(1, i)]))
+
 
 def downTi(x, i=0):
     if i < 0:
@@ -244,7 +273,7 @@ def downTi(x, i=0):
     elif i == 0 or i == 1:
         return 0
     else:
-        return downTi(x, i - 1) + math.prod([1 / (updi(x, -j) - 1) for j in range(1, i)])
+        return downTi(x, i - 1) + round_down(math.prod([1 / (updi(x, -j) - 1) for j in range(1, i)]))
 
 
 def Ai(x, i=0):
